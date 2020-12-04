3fb42be4a31f4d81c1fe5b9c6afe40fd186aeac3
Vivaldy Andhira <sayavivaldy@gmail.com>
2020-12-05 00:30:01 +0700

    fixed update query

--- tools/queries/queries.go
+++ tools/queries/queries.go
@@ -2,11 +2,11 @@ package queries
 
 const (
 	GET_ACCOUNT_BY_ACC_NUM = `SELECT * FROM tb_account WHERE account_number = ?`
-	TRANSFER_BALANCE = `UPDATE tb_account
-						SET balance = balance - ?
-						WHERE account_number = ?;
-						UPDATE tb_account
-						SET balance = balance + ?
-						WHERE account_number = ?;
-`
+	TRANSFER_BALANCE       = `UPDATE tb_account
+							  SET balance = CASE account_number
+										    WHEN ? THEN balance - ?
+										    WHEN ? THEN balance + ?
+										    ELSE balance
+										    END
+							  WHERE account_number IN(?, ?);`
 )
