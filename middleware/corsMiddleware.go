3b07d6f5b03b9e2f67c605fe27c63e1796076665
Vivaldy Andhira <sayavivaldy@gmail.com>
2020-12-04 19:39:07 +0700

    added activity log and CORS middleware

---
+++ middleware/corsMiddleware.go
@@ -0,0 +1,13 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		next.ServeHTTP(w, r)
+	})
+}
